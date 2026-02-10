aa2fa6680814107e5446edbe1d342e7ddd7ea16d
ruaridhg <ruaridhg@users.noreply.github.com>
2026-02-10 12:02:50 +0000

    Modify params for datatypes

--- tests/test_datatypes_matching.py
+++ tests/test_datatypes_matching.py
@@ -6,21 +6,25 @@ from xnat.session import XNATSession
 from xmigrate.main import check_datatypes_matching
 
 
-@pytest.mark.parametrize("xnatpy_connections", [
-    (
-        [
-            {"elementName": "xnat:mrSessionData"},
-            {"elementName": "xnat:petmrSessionDat"},
-            {"elementName": "xnat:ctSessionData"},
-            {"elementName": "proc:genProcData"},
-        ],
-        [
-            {"elementName": "xnat:mrSessionData"},
-            {"elementName": "xnat:petmrSessionDat"},
-            {"elementName": "xnat:ctSessionData"},
-        ]
-    )
-], indirect=True)
+@pytest.mark.parametrize(
+    "xnatpy_connections",
+    [
+        (
+            [
+                {"elementName": "xnat:mrSessionData"},
+                {"elementName": "xnat:petmrSessionDat"},
+                {"elementName": "xnat:ctSessionData"},
+                {"elementName": "proc:genProcData"},
+            ],
+            [
+                {"elementName": "xnat:mrSessionData"},
+                {"elementName": "xnat:petmrSessionDat"},
+                {"elementName": "xnat:ctSessionData"},
+            ],
+        )
+    ],
+    indirect=True,
+)
 def test_check_datatypes_matching_raises(xnatpy_connections: tuple[XNATSession, XNATSession]) -> None:
     """Test that check fails when source has datatypes not on destination."""
     source_conn, dest_conn = xnatpy_connections
@@ -29,20 +33,24 @@ def test_check_datatypes_matching_raises(xnatpy_connections: tuple[XNATSession, 
         check_datatypes_matching(source_conn, dest_conn)
 
 
-@pytest.mark.parametrize("xnatpy_connections", [
-    (
-        [
-            {"elementName": "xnat:mrSessionData"},
-            {"elementName": "xnat:petmrSessionDat"},
-            {"elementName": "xnat:ctSessionData"},
-        ],
-        [
-            {"elementName": "xnat:mrSessionData"},
-            {"elementName": "xnat:petmrSessionDat"},
-            {"elementName": "xnat:ctSessionData"},
-        ]
-    )
-], indirect=True)
+@pytest.mark.parametrize(
+    "xnatpy_connections",
+    [
+        (
+            [
+                {"elementName": "xnat:mrSessionData"},
+                {"elementName": "xnat:petmrSessionDat"},
+                {"elementName": "xnat:ctSessionData"},
+            ],
+            [
+                {"elementName": "xnat:mrSessionData"},
+                {"elementName": "xnat:petmrSessionDat"},
+                {"elementName": "xnat:ctSessionData"},
+            ],
+        )
+    ],
+    indirect=True,
+)
 def test_check_datatypes_matching_success(xnatpy_connections: tuple[XNATSession, XNATSession]) -> None:
     """Test that check passes when all source datatypes are on destination."""
     source_conn, dest_conn = xnatpy_connections
@@ -50,18 +58,22 @@ def test_check_datatypes_matching_success(xnatpy_connections: tuple[XNATSession,
     assert check_datatypes_matching(source_conn, dest_conn) is None
 
 
-@pytest.mark.parametrize("xnatpy_connections", [
-    (
-        [
-            {"elementName": "xdat:role_type"},
-            {"elementName": "xnat:petmrSessionDat"},
-        ],
-        [
-            {"elementName": "xdat:stored_search"},
-            {"elementName": "xnat:petmrSessionDat"},
-        ]
-    )
-], indirect=True)
+@pytest.mark.parametrize(
+    "xnatpy_connections",
+    [
+        (
+            [
+                {"elementName": "xdat:role_type"},
+                {"elementName": "xnat:petmrSessionDat"},
+            ],
+            [
+                {"elementName": "xdat:stored_search"},
+                {"elementName": "xnat:petmrSessionDat"},
+            ],
+        )
+    ],
+    indirect=True,
+)
 def test_check_datatypes_matching_ignores_xdat(xnatpy_connections: tuple[XNATSession, XNATSession]) -> None:
     """Test that xdat: prefixed datatypes are ignored."""
     source_conn, dest_conn = xnatpy_connections
