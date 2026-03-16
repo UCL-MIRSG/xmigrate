b0f766d06517584158af0a9be3ea43becb44a03a
Patrick J. Roddy <patrickjamesroddy@gmail.com>
2026-03-16 16:43:54 +0000

    Combine fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -16,112 +16,42 @@ if TYPE_CHECKING:
 
 
 @pytest.fixture(scope="session")
-def source_config() -> xnat4tests.Config:
-    """
-    Create configuration for the source XNAT instance.
-
-    Returns
-    -------
-        The configuration for the source XNAT instance.
-
-    """
-    return xnat4tests.Config(
-        xnat_root_dir=pathlib.Path(tempfile.mkdtemp()) / "source",
-        xnat_port="8888",
-        docker_image="xnat4tests_source",
-        docker_container="xnat4tests_source",
-    )
-
-
-@pytest.fixture(scope="session")
-def destination_config() -> xnat4tests.Config:
-    """
-    Create configuration for the destination XNAT instance.
-
-    Returns
-    -------
-        The configuration for the destination XNAT instance.
-
-    """
-    return xnat4tests.Config(
-        xnat_root_dir=pathlib.Path(tempfile.mkdtemp()) / "destination",
-        xnat_port="8889",
-        docker_image="xnat4tests_destination",
-        docker_container="xnat4tests_destination",
-    )
-
-
-@pytest.fixture(scope="session")
-def source_xnat(source_config: xnat4tests.Config) -> Generator[xnat4tests.Config, None, None]:
-    """
-    Start the source XNAT instance.
-
-    Parameters
-    ----------
-    source_config
-        The configuration for the source XNAT instance.
-
-    Returns
-    -------
-        The configuration for the source XNAT instance.
-
-    """
-    xnat4tests.start_xnat(source_config)
-    return source_config
-
-
-@pytest.fixture(scope="session")
-def destination_xnat(destination_config: xnat4tests.Config) -> Generator[xnat4tests.Config, None, None]:
-    """
-    Start the destination XNAT instance.
-
-    Parameters
-    ----------
-    destination_config
-        The configuration for the destination XNAT instance.
-
-    Returns
-    -------
-        The configuration for the destination XNAT instance.
-
-    """
-    xnat4tests.start_xnat(destination_config)
-    return destination_config
-
-
-@pytest.fixture(scope="session")
-def source_connection(source_xnat: xnat4tests.Config) -> Generator[xnat.BaseXNATSession, None, None]:
+def source_xnat() -> Generator[xnat.BaseXNATSession, None, None]:
     """
     Provide a connection to the source XNAT instance.
-
-    Parameters
-    ----------
-    source_xnat
-        The configuration for the source XNAT instance.
 
     Yields
     ------
         The active XNAT session for the source instance.
 
     """
-    with xnat4tests.connect(source_xnat) as conn:
+    config = xnat4tests.Config(
+        docker_container="xnat4tests_source",
+        docker_image="xnat4tests_source",
+        xnat_port="8888",
+        xnat_root_dir=pathlib.Path(tempfile.mkdtemp()) / "source",
+    )
+    xnat4tests.start_xnat(config)
+    with xnat4tests.connect(config) as conn:
         yield conn
 
 
 @pytest.fixture(scope="session")
-def destination_connection(destination_xnat: xnat4tests.Config) -> Generator[xnat.BaseXNATSession, None, None]:
+def destination_xnat() -> Generator[xnat.BaseXNATSession, None, None]:
     """
     Provide a connection to the destination XNAT instance.
-
-    Parameters
-    ----------
-    destination_xnat
-        The configuration for the destination XNAT instance.
 
     Yields
     ------
         The active XNAT session for the destination instance.
 
     """
-    with xnat4tests.connect(destination_xnat) as conn:
+    config = xnat4tests.Config(
+        docker_container="xnat4tests_destination",
+        docker_image="xnat4tests_destination",
+        xnat_port="8889",
+        xnat_root_dir=pathlib.Path(tempfile.mkdtemp()) / "destination",
+    )
+    xnat4tests.start_xnat(config)
+    with xnat4tests.connect(config) as conn:
         yield conn
