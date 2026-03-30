4eecc8b72dd45817089a79ac2e6a427d3db5893e
ruaridhg <ruaridhg@users.noreply.github.com>
2026-03-30 19:43:52 +0100

    Fix linting

--- tests/conftest.py
+++ tests/conftest.py
@@ -36,6 +36,8 @@ def remove_destination_test_data(
 
 @pytest.fixture(scope="session")
 def xnat_root_dirs(tmp_path_factory: pytest.TempdirFactory) -> dict[str, pathlib.Path]:
+    """Return fixed or temporary directories for source and destination xnat_root_dir."""
+
     def _xnat_root_dir(xnat_name: str) -> pathlib.Path:
         keep_instance = os.getenv("XNAT4TEST_KEEP_INSTANCE", "False").lower() == "true"
 
@@ -49,10 +51,8 @@ def xnat_root_dirs(tmp_path_factory: pytest.TempdirFactory) -> dict[str, pathlib
             xnat_root_dir = pathlib.Path(tmp_path_factory.mktemp(xnat_name))
 
         return xnat_root_dir
-    return {
-        "destination": _xnat_root_dir("destination"),
-        "source": _xnat_root_dir("source")
-    }
+
+    return {"destination": _xnat_root_dir("destination"), "source": _xnat_root_dir("source")}
 
 
 @pytest.fixture
