a8d8208777fdf2971f1c9087a8497c1f43708ca4
ruaridhg <ruaridhg@users.noreply.github.com>
2026-04-10 13:26:04 +0100

    Include json files if appearing under ASSESSORS folder

--- src/xmigrate/migration.py
+++ src/xmigrate/migration.py
@@ -895,7 +895,7 @@ class Migration:
                 "--exclude=*.log",
                 "--exclude=.*",
                 "--exclude=*.json",
-                "--stats",
+                "--include=*/ASSESSORS/*.json--stats",
                 "--progress",
                 "--checksum",
                 rsync_source,
