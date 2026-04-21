c8ff6ce4d1db141b52921b85f88fb12313b022bb
Patrick J. Roddy <patrickjamesroddy@gmail.com>
2026-04-21 11:42:23 +0100

    Create users fixtures

---
+++ tests/fixtures/users.py
@@ -0,0 +1,9 @@
+"""Fixures related to instance users."""
+
+import pytest
+
+
+@pytest.fixture
+def unique_username(request: pytest.FixtureRequest) -> str:
+    """Generate a unique username based on the test name."""
+    return request.node.name.replace("[", "_").replace("]", "_")
