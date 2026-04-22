9b23dd6c383ce2b1874edeb15954dc9141e00622
Patrick J. Roddy <patrickjamesroddy@gmail.com>
2026-04-22 16:05:39 +0100

    Sort plugins

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,8 @@
 """Main pytest configuration file."""
 
 pytest_plugins = [
-    "fixtures.xnat_config",
     "fixtures.connections",
-    "fixtures.project_info",
     "fixtures.helpers",
+    "fixtures.project_info",
+    "fixtures.xnat_config",
 ]
