84dd16140337d43e207762cd82614328d607e347
ruaridhg <ruaridhg@users.noreply.github.com>
2026-05-05 13:42:28 +0100

    Fix linting

--- tests/fixtures/miscellaneous.py
+++ tests/fixtures/miscellaneous.py
@@ -10,7 +10,7 @@ from tests._helper_functions import delete_data
 
 if typing.TYPE_CHECKING:
     import pathlib
-    from collections.abc import Generator
+    from collections.abc import Iterator
 
     import xnat
 
@@ -19,7 +19,7 @@ if typing.TYPE_CHECKING:
 def remove_destination_test_data(
     destination_connection: xnat.BaseXNATSession,
     xnat_root_dirs: dict[str, pathlib.Path],
-) -> Generator[None, None, None]:
+) -> Iterator[None]:
     """Fixture to delete data on destination and metadata dir e.g. output/localhost."""
     yield
     delete_data(destination_connection, xnat_root_dirs["destination"])
