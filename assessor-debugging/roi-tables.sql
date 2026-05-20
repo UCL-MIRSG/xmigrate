7908b72a3e30c7838e2f835b9ac30b516be90b1d
Paul Smith <paul.j.smith@ucl.ac.uk>
2026-05-20 07:34:57 +0100

    Temporarily add files for debugging assessor creation

---
+++ assessor-debugging/roi-tables.sql
@@ -0,0 +1,10 @@
+SELECT * FROM xnat_experimentdata;
+SELECT * FROM xnat_imageassessordata;
+SELECT * FROM icr_roicollectiondata;
+SELECT * FROM icr_roicollectiondata_seriesuid;
+SELECT * FROM xhbm_roi;
+SELECT * FROM xhbm_dicom_spatial_data;
+SELECT * FROM icr_roicollectiondata_meta_data;
+SELECT * FROM icr_roicollectiondata_seriesuid_meta_data;
+SELECT * FROM xnat_abstractresource_meta_data;
+
