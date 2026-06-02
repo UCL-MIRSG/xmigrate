1682596c784e9cbc549abb91090005d673a0f0bc
ruaridhg <ruaridhg@users.noreply.github.com>
2026-06-02 15:37:15 +0100

    Add postgres ssl connection test w/ updated tests wf

---
+++ tests/integration/test_postgres_ssl.py
@@ -0,0 +1,44 @@
+"""Tests for the xmigrate CLI."""
+
+import pathlib
+import shutil
+import typing
+
+import pytest
+
+import xnat
+
+import xmigrate.db as xdb
+from xmigrate.cli import app
+
+
+@pytest.mark.usefixtures("destination_connection")
+class TestPostgresAttachment:
+    """Test the migration of projects from source to destination XNAT."""
+
+    def test_attach_destination_database_with_ssl_client_cert(
+        self,
+        tmp_path: pathlib.Path,
+        monkeypatch: pytest.MonkeyPatch,
+    ) -> None:
+        configs_path = pathlib.Path(__file__).parent / "configs"
+
+        shutil.copy(
+            configs_path / "test_migrate_secrets_ssl.toml",
+            tmp_path / "secrets.toml",
+        )
+
+        monkeypatch.chdir(tmp_path)
+
+        conn = xdb.create_connection(":memory:")
+
+        try:
+            xdb.attach_destination_database(conn)
+
+            result = conn.execute(
+                "SELECT value FROM destination.public.test_table"
+            ).fetchone()
+
+            assert result == (1,)
+        finally:
+            conn.close()
